2a7bd2b928296c601b567bdac05ac2e8a43a3165
Gizza Az-Zahra <gizza24ti@mahasiswa.pcr.ac.id>
2025-12-02 22:00:37 +0700

    kerkom tdi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,23 +12,12 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-//     public function run(): void
-//     {
-//         // User::factory(10)->create();
-
-// <<<<<<< HEAD
-//        $this->call([
-//         AdminSeeder::class,
-//     ]);
-// =======
-//         User::factory()->create([
-//             'name' => 'Test User',
-//             'email' => 'test@example.com',
-//         ]);
-
-//         // seed menu items for pelanggan
-//         $this->call(\Database\Seeders\MenuSeeder::class);
-// >>>>>>> ca8de5f (Upload awal project ke branch gizza_pelanggan)
-//     }
-// }
+    public function run(): void
+    {
+        // Seed admin dan menu
+        $this->call([
+            AdminSeeder::class,
+            MenuSeeder::class,
+        ]);
+    }
 }
